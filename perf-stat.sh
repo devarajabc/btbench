15e2ebd4d4e56689f83b29f2f7df9fbe454e20d9
Leslie Zhai <zhaixiang@loongson.cn>
2025-10-22 14:52:18 +0800

    Add topdown for perf stat script

--- perf-stat.sh
+++ perf-stat.sh
@@ -1,12 +1,14 @@
-PERF="perf stat -e cpu-cycles -e instructions" 
 
 mkdir perf_stat_`date +%Y%m%d%H%M%S`
 cd perf_stat_`date +%Y%m%d%H%M%S`
 
+PERF="/your/path/linux-src/tools/perf/perf"
+
 rc=0
 while [[ $rc == 0 ]]
 do
-  out=$($PERF -p $1 sleep 1 2>&1|tee perf_`date +%Y%m%d%H%M%S`.log)
+  # `--topdown` and PMU events are only available for x86,  find some equivalence for your architecture. 
+  out=$($PERF stat --topdown -e cpu-cycles -e instructions -p $1 sleep 1 2>&1|tee perf_`date +%Y%m%d%H%M%S`.log)
   if [ `echo $out | grep -c "Usage" ` -gt 0 ]
   then
     rc=-1
